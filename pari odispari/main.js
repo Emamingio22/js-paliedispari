c2bdfd9b33ab85409878410397a5799e4613edcd
emamingio22 <emawankenoby02@gmail.com>
2023-04-05 16:28:12 +0200

    pari o dispari

--- pari odispari/main.js
+++ pari odispari/main.js
@@ -7,21 +7,56 @@ Dichiariamo chi ha vinto.
 */
 
 // CHIEDO ALL'UTENTE DI SCEGLIERE PARI O DISPARI
-let evenOrOdd = prompt("pari o dispari?");
-console.log(evenOrOdd);
+    let evenOrOdd = prompt("pari o dispari?");
+    console.log("hai scelto: ",evenOrOdd);
 
 
-let sceltaUtente;
+    let sceltaUtente;
 
-if(evenOrOdd === "pari"){
-    sceltaUtente = true;
-} else if(evenOrOdd === "dispari"){
-    sceltaUtente = false;
-}
+    if(evenOrOdd === "pari"){
+        sceltaUtente = true;
+    } else if(evenOrOdd === "dispari"){
+        sceltaUtente = false;
+    }
 
 // CHIEDO ALL'UTENTE DI SCEGLIERE UN NUMERO DA 1 A 5
-let numeroUtente = parseInt(prompt("scegli un numero da 1 a 5"));
-console.log(numeroUtente);
+    let numeroUtente = parseInt(prompt("scegli un numero da 1 a 5"));
+    console.log("hai scelto il numero: ",numeroUtente);
 
 // CHIEDO AL COMPUTER DI GENERARE UN NUMERO DA 1 A 5
-let numeroRandom = genNumbRandom(1, 5);
+    let numeroRandom = genNumRandom(1, 5);
+
+// SOMMO I DUE NUMERI (UTENTE E COMPUTER)
+    let somma = numeroUtente + numeroRandom;
+
+    console.log("il computer ha scelto il numero",numeroRandom );
+    console.log("la somma dei numeri è: ",somma);
+
+// SE LA SCELTA è UGUALE ALLA FUNZIONE numEven L'UTENTE HA VINTO
+    if(sceltaUtente == numeroPari(somma)){
+        console.log("HAI VINTO, COMPLIMENTI!!");
+    } else {
+        console.log("HAI PERSO, RIPROVA!!");
+    }
+
+
+
+
+// ----------FUNZIONI-----------
+
+// FUNZIONE PER GENERARE I NUMERI CASUALI
+    function genNumRandom(min, max){
+        return Math.floor(Math.random() * (max - min) + min);
+        
+    }
+
+
+// FUNZIONE NUMERO PARI
+    function numeroPari(numF){
+        if(numF % 2 == 0){
+            return even = true;
+        } else {
+            return even = false;
+        }
+    }
+

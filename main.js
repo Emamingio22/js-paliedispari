cb70256731b52fe4fb742717e6a474181bc07542
emamingio22 <emawankenoby02@gmail.com>
2023-04-05 14:33:22 +0200

    chiedo all'utente di inserire la parola

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-let parolaPalindroma = prompt ("inserisci una parola");
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma.
+*/
+
+// CHIEDO ALL'UTENTE DI INSERIRE UNA PAROLA
+const parolaUser = prompt("inserisci una parola");
+console.log(parolaUser);
+
